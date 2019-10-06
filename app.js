96dd072ab2e6877f8558c51ea50b337b3a9c8cb8
Tyler <tylerpoore2000@gmail.com>
2019-10-05 22:58:36 -0700

    First push on new node app

---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const port = 3000 || process.env.PORT;
+const apiKey = process.env.weatherApiKey;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/', (req, res) => {
+    let city = req.body.city,
+        url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+    request(url, (reqErr, reqRes, reqBody) => {
+       if(reqErr){
+           res.render('index', {weather: null, error: 'Error, please try again!'});
+       }else{
+           let weather = JSON.parse(reqBody);
+           if(weather.main == undefined){
+               res.render('index', {weather: null, error: 'Error, please try again!'});
+           }else{
+               let weatherText = `It's ${weather.main.temp} degress in ${weather.name}!`;
+               res.render('index', {weather: weatherText, error: null});
+           }
+        }
+    });
+});
